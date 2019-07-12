bfa3639f80ce0446dadc8ad53c617874cd1c81ce
cog1t0 <riku4724@gmail.com>
2019-07-13 07:24:46 +0900

    add webhook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  post '/webhook' => 'api/v1/webhook#webhook'
   resources :maps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
